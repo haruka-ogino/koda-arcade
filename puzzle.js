cd6a6256ba9afd1eea8d5ff70369942594001128
Haruka Ogino <harukariq@gmail.com>
2024-08-27 19:26:17 +1200

    make handle click fn

--- puzzle.js
+++ puzzle.js
@@ -4,11 +4,11 @@
 document.addEventListener('DOMContentLoaded', start)
 
 // line below grabs all the children from that classname:
-document.getElementsByClassName('board')[0].children
+// document.getElementsByClassName('board')[0].children
 
 function start() {
   const puzzlePieces = document.getElementsByClassName('board')[0].children
-  return puzzlePieces
+  bindEventListeners(puzzlePieces)
 }
 
 // event listener mouse events:
@@ -18,6 +18,33 @@ function start() {
 // click
 // dblclick
 // fn call is what you want to happen after the mouse event is experienced by the single piece position
+
+function bindEventListeners(pieces) {
+  for (let i = 0; i < pieces.length; i++) {
+    pieces[i].addEventListener('click', handleClick)
+  }
+}
+
+let firstClick
+let secondClick
+
+// toggle means if the class is there, remove it, otherwise, add it
+function handleClick(evt) {
+  evt.preventDefault()
+  const clickedPieces = document.getElementsByClassName('clicked')[0]
+  if (!clickedPieces) {
+    evt.target.classList.toggle('clicked')
+    firstClick = evt.srcElement.src
+    console.log('first click: ' + firstClick)
+  } else if (clickedPieces && clickedElement.children.length === 1) {
+    evt.target.classList.toggle('clicked')
+    secondClick = evt.srcElement.src
+    console.log('second click: ' + secondClick)
+    swapImgs()
+  } else if (clickedPieces && clickedElement.children.length > 2) {
+    return
+  }
+}
 
 // line below adds class
 // evt.target.classList.toggle([classname])
