385b529550122f76a7ef21d4370baeb317fd90ca
Haruka Ogino <harukariq@gmail.com>
2024-09-04 19:28:32 +1200

    create function to make initial array of images

--- scripts/jigsaw-puzzle.js
+++ scripts/jigsaw-puzzle.js
@@ -20,6 +20,9 @@ function start() {
     '014',
     '015',
   ]
+
+  const two = 2
+  const newNum = two.toString()
   // randomise fn to be built
   for (let i = 0; i < images.length; i++) {
     const img = document.createElement('img')
@@ -27,5 +30,20 @@ function start() {
     img.alt = 'puzzle piece'
     img.width = 200
     imageContainer.appendChild(img)
+  }
+}
+
+function createArr() {
+  const images = []
+
+  for (let i = 0; i < 15; i++) {
+    const number = toString(i + 1)
+    let element
+    if (i < 9) {
+      element = `00${number}`
+    } else {
+      element = `0${number}`
+    }
+    images.push(element)
   }
 }
