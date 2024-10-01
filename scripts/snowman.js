6593d987193520b6c05791b075bfad7f5e5b5ba1
Haruka Ogino <harukariq@gmail.com>
2024-10-01 17:33:07 +1300

    set up snowman game

---
+++ scripts/snowman.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', startGame)
+
+function startGame() {
+  const answer = 'CAMEL'
+
+  displayAlphabet()
+}
+
+function displayAlphabet() {
+  const letters = [
+    { letter: 'A', show: true },
+    { letter: 'B', show: true },
+    { letter: 'C', show: true },
+    { letter: 'D', show: true },
+    { letter: 'E', show: true },
+    { letter: 'F', show: true },
+    { letter: 'G', show: true },
+    { letter: 'H', show: true },
+    { letter: 'I', show: true },
+    { letter: 'J', show: true },
+    { letter: 'K', show: true },
+    { letter: 'L', show: true },
+    { letter: 'M', show: true },
+    { letter: 'N', show: true },
+    { letter: 'O', show: true },
+    { letter: 'P', show: true },
+    { letter: 'Q', show: true },
+    { letter: 'R', show: true },
+    { letter: 'S', show: true },
+    { letter: 'T', show: true },
+    { letter: 'U', show: true },
+    { letter: 'V', show: true },
+    { letter: 'W', show: true },
+    { letter: 'X', show: true },
+    { letter: 'Y', show: true },
+    { letter: 'Z', show: true },
+  ]
+}
